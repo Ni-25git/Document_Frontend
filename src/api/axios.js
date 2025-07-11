2e0a8766065df2c2f2fb28a450281566922750d7
Ni-25git <nipunsherawat@gmail.com>
2025-07-11 10:21:49 +0530

    Document Frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://document-backend-asee.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://document-backend-1-myez.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
